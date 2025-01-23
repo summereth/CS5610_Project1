a77e0ff9dbcb4cf2df8217d9bfdc660ab87bcb60
summereth <liq.qian.l@gmail.com>
2025-01-22 16:26:04 -0800

    Modify eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,48 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import pluginReact from 'eslint-plugin-react';
+import prettier from "eslint-config-prettier";
+import js from "@eslint/js";
+import { includeIgnoreFile } from "@eslint/compat";
+// import svelte from "eslint-plugin-svelte";
+import globals from "globals";
+import { fileURLToPath } from "node:url";
+const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
 
-/**  @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['/*.{js,mjs,cjs,jsx}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  // ...svelte.configs["flat/recommended"],
+  prettier,
+  // ...svelte.configs["flat/prettier"],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    rules: {
+      indent: [
+        "error",
+        2,
+        {
+          SwitchCase: 1,
+        },
+      ],
+
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+      "no-console": 0,
+    },
+  },
 ];
